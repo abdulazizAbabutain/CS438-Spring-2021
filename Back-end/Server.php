f0e0287fe0ece1faa4da026f1f88282a0ad8453c
Abdulaziz Ababutain <k.k.m1432@gmail.com>
2021-03-22 20:29:39 +0300

    add new html files

--- Back-end/Server.php
+++ Back-end/Server.php
@@ -3,28 +3,28 @@
 //user registration data
 $username = $_POST['username'];
 $email = $_POST['email'];
-$password = $_POST['password']; 
+$password = $_POST['password'];
 
 //database access data
 $servername = "localhost";
-$dbusername= "root";
-$dbpassword= "root";
+$dbusername = "root";
+$dbpassword = "root";
 $dbname = "registration";
 
 //creating connection
-$conn = new mysqli($servername,$dbusername,$dbpassword,$dbname);
+$conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
 //check if connected or not
-if($conn->connect_error){
-die("connection failed" . $conn->connect_error);
+if ($conn->connect_error) {
+    die("connection failed" . $conn->connect_error);
 }
 //regestring users into database
 $sql = "INSERT INTO user(username, email, password)VALUES ('$username','$email','$password')";
 //checking if the query was successfully added or not
-if($conn->query($sql)=== TRUE){
+if ($conn->query($sql) === TRUE) {
 
     echo "added";
-} else {echo "error";}
+} else {
+    echo "error";
+}
 
 $conn->close();
-
-?>
