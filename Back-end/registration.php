97dd22533d4d3538e11a791467890549c766863d
AbdulrahmanBalood <53280855+AbdulrahmanBalood@users.noreply.github.com>
2021-04-05 20:42:04 +0300

    Update registration.php

--- Back-end/registration.php
+++ Back-end/registration.php
@@ -1,4 +1,5 @@
 <?php
+session_start(); 
 
 //database access data
 $servername = "localhost";
@@ -22,22 +23,26 @@ $duplicate_username = "SELECT username FROM user WHERE username = '$username'";
 $user_result = mysqli_query($conn ,$duplicate_username);//
 $user_count = mysqli_num_rows($user_result);
 if($user_count > 0){
-  echo "username already exist";
+    echo '<script>alert("username already exist");
+    window.location.href="Front-end/Home_page/Registration_page/Registration.html";
+    </script>';
   return false;
 }
 $duplicate_email = "SELECT email FROM user WHERE email = '$email'";
 $email_result = mysqli_query($conn ,$duplicate_email);
 $email_count = mysqli_num_rows($email_result);
 if($email_count > 0){
-  echo "email already exist";
+  echo '<script>alert("email already exist");
+  window.location.href="Front-end/Home_page/Registration_page/Registration.html";
+  </script>';
   return false;
 }
 //regestring users into database
-$sql = "INSERT INTO user(username, email, password)VALUES ('$username','$email','$password')";
+$sql = "INSERT INTO user(username, email, password, score)VALUES ('$username','$email','$password', 0)";
 //if the registraion is correct the user will be directed to the login page, if not it will display error message 
 if ($conn->query($sql) === TRUE) {
 
-    header("location: Login.html");
+    header("location: /Front-end/Home_page/Log_in_page/Login.html");
 } else {
     echo "error";
 }
